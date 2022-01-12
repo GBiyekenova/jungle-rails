65f3f13521f9e5c4242c28147c3b9e44c02a5795
Gulbike Biyekenova <gbknva@gmail.com>
2022-01-12 19:18:37 +0000

    specs: product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  scenario "They see product description" do
+    visit root_path
+    click_on 'Details'
+
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+
+    # commented out b/c it's for debugging only
+    save_and_open_screenshot
+
+    expect(page).to have_content('Description')
+    expect(page).to have_content('Name')
+  end
+
+end
